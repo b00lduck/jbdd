fa79d95fcdacc3f54b621ef91ef18abcec703c17
daniel <daniel@zerlett.eu>
2015-01-28 20:49:22 +0100

    cost editor

--- jsclient/app/modules/admin/modules/buyableCostEditor/controllers/buyableCostEditor.js
+++ jsclient/app/modules/admin/modules/buyableCostEditor/controllers/buyableCostEditor.js
@@ -10,21 +10,29 @@ define(['angularAMD', 'DataService'], function (angularAMD) {
             var addableGoods = [];
             var buyableCosts = [];
 
-            DataService.getAddableCostGoods($scope.resourceName, $scope.getBuyableId()).then(function (payload) {
-                addableGoods = payload.data;
-            });
+            function refresh() {
+                DataService.getAddableCostGoods($scope.resourceName, $scope.getBuyableId()).then(function (payload) {
+                    addableGoods = payload.data;
+                });
 
-            DataService.getBuyableCosts($scope.resourceName, $scope.getBuyableId()).then(function (payload) {
-                buyableCosts = payload.data.data;
-                $scope.gridOptions.data = buyableCosts;
-            });
+                DataService.getBuyableCosts($scope.resourceName, $scope.getBuyableId()).then(function (payload) {
+                    buyableCosts = payload.data.data;
+                    $scope.gridOptions.data = buyableCosts;
+                });
+            }
+
+            refresh();
 
             $scope.getAddableCostGoods = function () {
                 return addableGoods.data;
             };
 
             $scope.addCost = function () {
-                DataService.addCostToBuyable($scope.resourceName, $scope.getBuyableId(), $scope.selectedGood.id);
+                DataService.addCostToBuyable($scope.resourceName, $scope.getBuyableId(), $scope.selectedGood.id).then(function () {
+                    refresh();
+                }).catch(function () {
+                    alert(1);
+                });
             };
 
             $scope.removeCost = function () {
