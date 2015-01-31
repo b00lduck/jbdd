24cf167b75362bf90e0793f348eb8f4c11be6a0a
daniel <daniel@zerlett.eu>
2015-01-31 22:00:30 +0100

    moved html templates to file

--- jsclient/app/modules/admin/modules/constants/main.js
+++ jsclient/app/modules/admin/modules/constants/main.js
@@ -1,16 +1,24 @@
 /*jslint node: true */
 'use strict';
 
-
-define([], function () {
+define(['text!./templates/cellTemplateI18nField.html',
+        'text!./templates/editCellTemplateNonZeroInt.html',
+        'text!./templates/enabledCellTemplate.html',
+        'text!./templates/enabledRowTemplate.html',
+        'text!./templates/deletableCellTemplate.html'],
+    function (cellTemplateI18nField,
+              editCellTemplateNonZeroInt,
+              enabledCellTemplate,
+              enabledRowTemplate,
+              deletableCellTemplate) {
 
     return {
         templates: {
-            cellTemplateI18nField: '<div class="ui-grid-cell-contents">{{getExternalScopes().getFieldByLanguage(COL_FIELD)}}</div>',
-            editCellTemplateNonZeroInt: '<div><form name="inputForm"><input type="number" required="true" min="1" pattern="^[0-9]+$" ng-class="\'colt\' + col.uid" ui-grid-editor ng-model="MODEL_COL_FIELD"></form></div>',
-            enabledCellTemplate: '<div class="ui-grid-cell-contents ng-binding ng-scope"><span class="glyphicon glyphicon-{{ COL_FIELD == true ? \'ok-circle\' : \'ban-circle\'}}" aria-hidden="true"></span></div>',
-            enabledRowTemplate: '<div ng-dblClick="getExternalScopes().editItem(row.entity)" ng-class="{\'red\':row.entity.enabled == false, \'green\':row.entity.enabled == true }"><div ng-repeat="(colRenderIndex, col) in colContainer.renderedColumns track by col.colDef.name" class="ui-grid-cell" ng-class="{ \'ui-grid-row-header-cell\': col.isRowHeader }" ui-grid-cell></div></div>',
-            deletableCellTemplate: '<div class="ui-grid-cell-contents ng-binding ng-scope"><button ng-if="COL_FIELD" class="ng-binding" ng-click="getExternalScopes().deleteItem(row.entity)">delete</button></div>'
+            cellTemplateI18nField: cellTemplateI18nField,
+            editCellTemplateNonZeroInt: editCellTemplateNonZeroInt,
+            enabledCellTemplate: enabledCellTemplate,
+            enabledRowTemplate: enabledRowTemplate,
+            deletableCellTemplate: deletableCellTemplate
         }
     };
 
