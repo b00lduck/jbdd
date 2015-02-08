776d21a08b74620591877aab93f8a3acd24f0b95
daniel <daniel@zerlett.eu>
2015-02-08 16:41:38 +0100

    swagger config refactoring

--- com.nigames.jbdd.rest.service/src/main/java/com/nigames/jbdd/service/config/SwaggerConfig.java
+++ com.nigames.jbdd.rest.service/src/main/java/com/nigames/jbdd/service/config/SwaggerConfig.java
@@ -3,10 +3,15 @@ package com.nigames.jbdd.service.config;
 import com.wordnik.swagger.jaxrs.config.BeanConfig;
 import com.wordnik.swagger.jaxrs.json.JacksonJsonProvider;
 import com.wordnik.swagger.jaxrs.listing.ApiDeclarationProvider;
+import com.wordnik.swagger.jaxrs.listing.ApiListingResourceJSON;
 import com.wordnik.swagger.jaxrs.listing.ResourceListingProvider;
 import org.jboss.resteasy.spi.ResteasyProviderFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+import javax.ws.rs.Path;
 
 /**
  * This file is part of JBdD by nigames.de
@@ -14,6 +19,8 @@ import org.springframework.context.annotation.Configuration;
  * Created by Daniel on 08.02.2015.
  */
 @Configuration
+@ComponentScan(basePackageClasses = ApiListingResourceJSON.class,
+		includeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, value = Path.class))
 public class SwaggerConfig {
 
 	@Bean
@@ -24,7 +31,9 @@ public class SwaggerConfig {
 		ResteasyProviderFactory.getInstance().registerProvider(ResourceListingProvider.class);
 
 		BeanConfig beanConfig = new BeanConfig();
-		beanConfig.setVersion("v1");
+		beanConfig.setVersion("1.0.0");
+		beanConfig.setDescription("RESTful services API for \"Das Buch des Drachen\"");
+		beanConfig.setTitle("JBdD REST API");
 		beanConfig.setBasePath("http://localhost:8080");
 		beanConfig.setResourcePackage("com.nigames.jbdd.rest.api");
 		beanConfig.setScan(true);
