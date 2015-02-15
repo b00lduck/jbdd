e5177e622f146e9bb18754b5c3fce6f9dee1556c
daniel <daniel@zerlett.eu>
2015-02-15 12:31:50 +0100

    list decorator pattern for ResultList

--- com.nigames.jbdd.rest.model/src/main/java/com/nigames/jbdd/types/ResultList.java
+++ com.nigames.jbdd.rest.model/src/main/java/com/nigames/jbdd/types/ResultList.java
@@ -1,171 +1,41 @@
 package com.nigames.jbdd.types;
 
-import java.util.*;
-import java.util.function.UnaryOperator;
+import com.google.common.collect.ForwardingList;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
- * Created by developer on 2/8/15.
+ * This file is part of JBdD by nigames.de
+ *
+ * Created by Daniel on 15.02.2015.
  */
-public class ResultList<DtoType> implements List<DtoType> {
+public class ResultList<DtoType> extends ForwardingList<DtoType> {
 
     private final List<DtoType> list;
 
     private final long count;
 
-    public static <DtoType> ResultList<DtoType> create(final List<DtoType> list) {
-        return create(list, list.size());
+	private ResultList(final List<DtoType> list, final long aCount) {
+		this.list = Collections.unmodifiableList(list);
+		count = aCount;
+	}
+
+	public static <DtoType> ResultList<DtoType> create(final List<DtoType> list) {
+		return create(list, list.size());
     }
 
     public static <DtoType> ResultList<DtoType> create(final List<DtoType> list, final long total) {
         return new ResultList<>(list, total);
     }
 
-    private ResultList(final List<DtoType> list, final long aCount) {
-        this.list = list;
-        count = aCount;
-    }
-
-    @Override
-    public int size() {
-        return list.size();
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return list.isEmpty();
-    }
-
-    @Override
-    public boolean contains(Object o) {
-        return list.contains(o);
-    }
-
-    @Override
-    public Iterator<DtoType> iterator() {
-        return list.iterator();
-    }
-
-    @Override
-    public Object[] toArray() {
-        return list.toArray();
-    }
-
-    @Override
-    public <T> T[] toArray(T[] a) {
-        return list.toArray(a);
-    }
-
-    @Override
-    public boolean add(DtoType dtoType) {
-        return list.add(dtoType);
-    }
-
-    @Override
-    public boolean remove(Object o) {
-        return list.remove(o);
-    }
-
-    @Override
-    public boolean containsAll(Collection<?> c) {
-        return list.containsAll(c);
-    }
-
-    @Override
-    public boolean addAll(Collection<? extends DtoType> c) {
-        return list.addAll(c);
-    }
-
-    @Override
-    public boolean addAll(int index, Collection<? extends DtoType> c) {
-        return list.addAll(index, c);
-    }
-
-    @Override
-    public boolean removeAll(Collection<?> c) {
-        return list.removeAll(c);
-    }
-
-    @Override
-    public boolean retainAll(Collection<?> c) {
-        return list.retainAll(c);
-    }
-
-    @Override
-    public void replaceAll(UnaryOperator<DtoType> operator) {
-        list.replaceAll(operator);
-    }
-
-    @Override
-    public void sort(Comparator<? super DtoType> c) {
-        list.sort(c);
-    }
-
-    @Override
-    public void clear() {
-        list.clear();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        return list.equals(o);
-    }
-
-    @Override
-    public int hashCode() {
-        return list.hashCode();
-    }
-
-    @Override
-    public DtoType get(int index) {
-        return list.get(index);
-    }
-
-    @Override
-    public DtoType set(int index, DtoType element) {
-        return list.set(index, element);
-    }
-
-    @Override
-    public void add(int index, DtoType element) {
-        list.add(index, element);
-    }
-
-    @Override
-    public DtoType remove(int index) {
-        return list.remove(index);
-    }
-
-    @Override
-    public int indexOf(Object o) {
-        return list.indexOf(o);
-    }
-
-    @Override
-    public int lastIndexOf(Object o) {
-        return list.lastIndexOf(o);
-    }
-
-    @Override
-    public ListIterator<DtoType> listIterator() {
-        return list.listIterator();
-    }
-
-    @Override
-    public ListIterator<DtoType> listIterator(int index) {
-        return list.listIterator(index);
-    }
-
-    @Override
-    public List<DtoType> subList(int fromIndex, int toIndex) {
-        return list.subList(fromIndex, toIndex);
-    }
-
-    @Override
-    public Spliterator<DtoType> spliterator() {
-        return list.spliterator();
-    }
+	@Override
+	protected List<DtoType> delegate() {
+		return list;
+	}
 
     public long getTotalCount() {
         return count;
     }
+
 }
--- com.nigames.jbdd.rest.model/src/test/groovy/com/nigames/jbdd/types/ResultListSpec.groovy
+++ com.nigames.jbdd.rest.model/src/test/groovy/com/nigames/jbdd/types/ResultListSpec.groovy
@@ -1,9 +1,6 @@
 package com.nigames.jbdd.types
 
 import spock.lang.Specification
-import spock.lang.Unroll
-
-import java.util.function.UnaryOperator
 
 /**
  * This file is part of JBdD by nigames.de
@@ -12,145 +9,53 @@ import java.util.function.UnaryOperator
  */
 class ResultListSpec extends Specification {
 
-    @Unroll
-    def "delegate #delegateName to ArrayList"() {
+    def "constructor with list und totalCount"() {
 
         given:
-        def list = Mock(List)
-        def testSubject = new ResultList(list, 55)
+        def array = new ArrayList<String>()
+        array.add("a")
+        array.add("b")
+        array.add("c")
 
         when:
-        def retVal = testSubject.invokeMethod(delegateName, null)
+        def testSubject = ResultList.create(array, 10)
 
         then:
-        1 * list./$delegateName/() >> ret
-
-        where:
-        delegateName   | ret
-        "size"         | 20
-        "isEmpty"      | true
-        "isEmpty"      | false
-        "iterator"     | Mock(Iterator)
-        "toArray"      | ['1', '2']
-        "clear"        | null
-        "hashCode"     | 1337
-        "spliterator"  | Mock(Spliterator)
-        "listIterator" | Mock(ListIterator)
-
+        testSubject.getTotalCount() == 10
+        testSubject.size() == 3
+        noExceptionThrown()
     }
 
-    @Unroll
-    def "delegate #delegateName(#params) to ArrayList"() {
+    def "constructor with list"() {
 
         given:
-        def list = Mock(List)
-        def testSubject = new ResultList(list, 0)
+        def array = new ArrayList<String>()
+        array.add("a")
+        array.add("b")
+        array.add("c")
 
         when:
-        def retVal = testSubject.invokeMethod(delegateName, params)
+        def testSubject = ResultList.create(array)
 
         then:
-        1 * list./$delegateName/(params) >> ret
-        retVal == ret
-
-        where:
-        delegateName   | params              | ret
-        "contains"     | [new Object()]      | true
-        "toArray"      | [['1', '2']]        | ['3', '4']
-        "add"          | [new Object()]      | false
-        "remove"       | [new Object()]      | false
-        "containsAll"  | [['1', '2']]        | false
-        "addAll"       | [['1', '2']]        | false
-        "removeAll"    | [['1', '2']]        | false
-        "retainAll"    | [['1', '2']]        | false
-        "replaceAll"   | Mock(UnaryOperator) | null
-        "sort"         | Mock(Comparator)    | null
-        "equals"       | new Object()        | true
-        "get"          | 1                   | new Object()
-        "remove"       | 1                   | new Object()
-        "indexOf"      | [new Object()]      | 1
-        "lastIndexOf"  | [new Object()]      | 1
-        "listIterator" | 1                   | Mock(ListIterator)
-
+        testSubject.getTotalCount() == 3
+        testSubject.size() == 3
+        noExceptionThrown()
     }
 
-    def "delegate subList ( 1, 2) to ArrayList"() {
+    def "adding fails"() {
 
         given:
-        def list = Mock(List)
-        def testSubject = new ResultList(list, 0)
-        def ret = Mock(List)
+        def array = new ArrayList<String>()
+        def testSubject = ResultList.create(array)
 
         when:
-        def retVal = testSubject.subList(1, 2)
+        testSubject.add("a")
 
         then:
-        1 * list.subList(1, 2) >> ret
-        retVal == ret
-
+        thrown(UnsupportedOperationException)
     }
 
-    def "delegate add ( 1, obj) to ArrayList"() {
-
-        given:
-        def list = Mock(List)
-        def testSubject = new ResultList(list, 0)
-        def obj = new Object()
-
-        when:
-        def retVal = testSubject.add(1, obj)
-
-        then:
-        1 * list.add(1, obj)
-
-    }
-
-    def "delegate set ( 1, obj) to ArrayList"() {
-
-        given:
-        def list = Mock(List)
-        def testSubject = ResultList.create(list, 0)
-        def ret = Mock(List)
-        def obj = new Object()
-
-        when:
-        def retVal = testSubject.set(1, obj)
-
-        then:
-        1 * list.set(1, obj) >> ret
-        retVal == ret
-
-    }
-
-
-    def "delegate addAll ( 1, obj) to ArrayList"() {
-
-        given:
-        def list = Mock(List)
-        def testSubject = ResultList.create(list)
-        def obj = Mock(List)
-
-        when:
-        def retVal = testSubject.addAll(1, obj)
-
-        then:
-        1 * list.addAll(1, obj) >> true
-        retVal == true
-
-    }
-
-    def "return getTotalCount ()"() {
-
-        given:
-        def testSubject = ResultList.create(null, 666)
-
-        when:
-        def retVal = testSubject.getTotalCount()
-
-        then:
-        retVal == 666
-
-    }
 
 }
 
