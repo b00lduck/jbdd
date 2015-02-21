42d93686a8c76da8805103071ce4d7b124905ecd
Gregor Anders <gregor.anders@gmail.com>
2015-02-21 19:27:26 +0100

    hasCode/euals testing framework disallows testing of abstract method implementations

--- com.nigames.jbdd.rest.service/src/test/groovy/com/nigames/jbdd/domain/entities/facet/identifyable/IdentifyableEntityFacetImplSpec.groovy
+++ com.nigames.jbdd.rest.service/src/test/groovy/com/nigames/jbdd/domain/entities/facet/identifyable/IdentifyableEntityFacetImplSpec.groovy
@@ -1,7 +1,5 @@
 package com.nigames.jbdd.domain.entities.facet.identifyable
 
-import nl.jqno.equalsverifier.EqualsVerifier
-import nl.jqno.equalsverifier.Warning
 import spock.lang.Specification
 
 /**
@@ -10,13 +8,6 @@ import spock.lang.Specification
  * Created by Daniel on 20.02.2015.
  */
 class IdentifyableEntityFacetImplSpec extends Specification {
-
-    def "equals and hashcode contract"() {
-        expect:
-        EqualsVerifier.forClass(IdentifyableEntityFacetImpl)
-                .suppress(Warning.NONFINAL_FIELDS)
-                .verify()
-    }
 
 
     def "id getter"() {
